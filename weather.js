ef07e878c692571a0b2d510b676c416dd1251fdb
Grant Winter <grantrwinter@gmail.com>
2014-01-14 23:59:06 +0000

    functional working version of project(WIP-add style)

--- weather.js
+++ weather.js
@@ -1,42 +1,39 @@
 $(document).ready(function() {
-    // log in here
-   
 
-   
-
-
-
-  //url maker
-   /* var urlMaker = function(text){
-      return ;
-    };
-    console.log(urlMaker("stuff"));*/
-    
-    
    $('#cities').click(function() {
     var text =  $("#cityInput").val();
     var url = 'http://autocomplete.wunderground.com/aq?query=' + text + '&cb=?';
     console.log(url);
-    $.getJSON(url, function(data){
-        var cities = data.RESULTS;
-        console.log( cities );
-        for(var i=0; i < cities.length; i++) {
-          var city = cities[i];          
-          $("#cityData").append("<li data-url='http://api.wunderground.com/api/e401293040a26d38/forecast'" + city.l + "<li>" + city.name + "</li>");  
-          console.log(city.l); 
-        } 
 
-        var lis = $("li");
-
-        /*for (var j = 0; j < lis.length; j++ ) {
-          lis[j].click(function() {
-              $.getJSON()
-          }
-        }*/
-          
-      
+  $.getJSON(url, function(data){
+      var cities = data.RESULTS;
+      console.log( cities );
+      for(var i=0; i < cities.length; i++) {
+        var city = cities[i];  
+        var key = "e401293040a26d38"        
+        $("ul").append("<li data-url='" + "http://api.wunderground.com/api/" + key + "/forecast" + city.l + ".json'>" + city.name + "</li>");  
+      } 
     }); 
-  }); 
+  });
+
+  $("body").delegate('li', 'click', function(){
+          var url = $(this).attr("data-url");
+
+          $.ajax({
+            url: url,
+            dataType: "jsonp",
+            success: function(data) {
+              console.log(data);
+              $("#weatherReport").append("<p>" + data.forecast.txt_forecast.forecastday[0].fcttext +"</p>");
+
+              //add icon image
+              $("#icon").append("<div>" + data.forecast.txt_forecast.forecastday[0].icon_url +"</div>");
+            }
+          })
+      });
+
+  function weather(data) {
+    
+  }
 });
-//    http://api.wunderground.com/api/e401293040a26d38/forecast/' + city.l
 
